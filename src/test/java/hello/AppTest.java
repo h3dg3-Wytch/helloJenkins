0b263f7e0d7f3c19e62c3cf3e597b53a23efc637
Rex Petersen <rex.petersen@dal-10357-wl.ad.kibocommerce.com>
2017-09-07 14:20:15 -0500

    Don't mind me, in and out

---
+++ src/test/java/hello/AppTest.java
@@ -0,0 +1,17 @@
+package hello;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    @Test
+    public void TestIfTestWork(){
+        assert true;
+    }
+
+}
